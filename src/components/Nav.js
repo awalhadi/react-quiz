4e3c48713cac1d8ef31f2718514dc12eb90cd140
A Awal Hadi <awalhadi5@gmail.com>
2022-10-16 21:40:09 +0600

    initial home page transfer from html complete

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import logo from "../assets/images/logo-bg.png";
+import styles from "../styles/Nav.module.css";
+import Account from "./Account";
+
+export default function Nav(){
+    return (
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                <a href="index.html" className={styles.brand}>
+                    <img src={logo} alt="Learn with Sumit Logo" />
+                    <h3>Learn with Sumit</h3>
+                </a>
+                </li>
+            </ul>
+            <Account />
+        </nav>
+    );
+} 
